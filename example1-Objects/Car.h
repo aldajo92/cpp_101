f6bd8623d59d6197c97ddbc22248593fd5eee2e1
aldajo92 <alejodiscovery20@gmail.com>
2017-02-16 22:45:52 -0500

    rename folders

---
+++ example1-Objects/Car.h
@@ -0,0 +1,18 @@
+/*
+ * Car.h
+ *
+ *  Created on: Jan 27, 2017
+ *      Author: algomez
+ */
+
+#ifndef CAR_H_
+#define CAR_H_
+
+class Car {
+public:
+	Car();
+	void run();
+	void stop();
+};
+
+#endif /* CAR_H_ */
---
+++ example1-Objects/main.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : cpp_101.cpp
+// Author      : Alejandro Gomez
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include "Car.h"
+using namespace std;
+
+int main() {
+	Car car;
+	car.stop();
+	car.run();
+	return 0;
+}
